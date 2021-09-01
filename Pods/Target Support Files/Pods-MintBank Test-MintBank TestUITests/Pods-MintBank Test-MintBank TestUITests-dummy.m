03fd7d73fc71fe5b0d8b456e0ec841d0ec989a3e
Temitope Adebiyi <adebiyitope26@gmail.com>
2021-09-01 13:46:36 +0100

    additional files

---
+++ Pods/Target Support Files/Pods-MintBank Test-MintBank TestUITests/Pods-MintBank Test-MintBank TestUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MintBank_Test_MintBank_TestUITests : NSObject
+@end
+@implementation PodsDummy_Pods_MintBank_Test_MintBank_TestUITests
+@end
---
+++ Pods/Target Support Files/Pods-MintBank TestTests/Pods-MintBank TestTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MintBank_TestTests : NSObject
+@end
+@implementation PodsDummy_Pods_MintBank_TestTests
+@end
